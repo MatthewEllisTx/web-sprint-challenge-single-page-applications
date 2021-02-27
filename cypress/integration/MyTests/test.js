f1a93e5a9fad8ee83ea42c549bc716e72e5a9506
Matthew Ellis <mbellistx@gmail.com>
2021-02-26 19:14:45 -0600

    moved files to right spot

---
+++ cypress/integration/MyTests/test.js
@@ -0,0 +1,20 @@
+describe('forms test', () => {
+  
+  beforeEach( () => {
+    cy.visit('https://localhost:3000')
+  })
+  
+  const url = () => cy.url()
+  const 1 = () => cy.get(2)
+  const 1 = () => cy.get(2)
+  const 1 = () => cy.get(2)
+  const 1 = () => cy.get(2)
+  const 1 = () => cy.get(2)
+  const 1 = () => cy.get(2)
+  const 1 = () => cy.get(2)
+  const 1 = () => cy.get(2)
+
+  it('check present buttons on / and /pizza', () => {
+    
+  })
+})
